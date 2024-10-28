802c2bf07946968dcd826bc08b7c98b57cb57488
Ashutosh pandey <ashutosh_pandey@prescienceds.com>
2024-10-28 14:14:31 +0530

    Update code

--- model_checker/check_model.py
+++ model_checker/check_model.py
@@ -28,9 +28,33 @@ def load_llama_model():
     except Exception as e:
         logging.error("Error loading Llama model: %s", e)
 
+def load_evaluation_model():
+    ''' Load llama model from huggingface'''
+    try:
+        model = "meta-llama/Meta-Llama-3-8B-Instruct"
+        tokenizer = AutoTokenizer.from_pretrained(model)
+
+        pipe = pipeline(
+            "text-generation",
+            model=model,
+            tokenizer=tokenizer,
+            torch_dtype=torch.bfloat16,
+            trust_remote_code=True,
+            device_map="auto",
+            do_sample=True,
+            top_k=10,
+            num_return_sequences=1,
+            eos_token_id=tokenizer.eos_token_id,
+        )
+
+        return pipe
+    except Exception as ex:
+        logging.error("Error loading evaluation model: %s", ex)
+        raise ex
+
 
 def save_model(model_path):
-    pipe= load_llama_model()
+    pipe= load_evaluation_model()
     pipe.save_pretrained(model_path)
     model_pipe = pipeline("text-classification", model = model_path)
     logging.info("model_loaded successfully")
