d00e733758b103d20608420f53db67514f288eac
Ashutosh pandey <ashutosh_pandey@prescienceds.com>
2024-12-13 14:22:38 +0530

    Remove unwanted code

--- model_checker/check_model.py
+++ model_checker/check_model.py
@@ -1,10 +1,7 @@
 import os
-import re
 import torch
 import logging
 from transformers import AutoTokenizer, pipeline
-from langchain.prompts import PromptTemplate
-from langchain import HuggingFacePipeline
 
 def load_llama_model():
     try:
@@ -54,7 +51,20 @@ def load_evaluation_model():
 
 
 def save_model(model_path):
-    pipe= load_evaluation_model()
+    pipe= load_llama_model()
     pipe.save_pretrained(model_path)
     model_pipe = pipeline("text-classification", model = model_path)
-    logging.info("model_loaded successfully")
+    logging.info("LLM model loaded successfully")
+
+def save_eval_model(model_path):
+    logging.info("Initialized loading evaluation model")
+    pipe= load_evaluation_model()
+    pipe.save_pretrained(model_path)
+    eval_pipe = pipeline("text-classification", model = model_path)
+    logging.info("Evaluation model loaded successfully")
+
+if __name__=='__main__':
+    model_path= os.path.join("model")
+    save_model(model_path)
+    evaluation_path= os.path.join("evaluation_model")
+    save_eval_model(evaluation_path)
